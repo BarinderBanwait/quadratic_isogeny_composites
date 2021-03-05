592ebdd940f88e0490f4f16b11de5cd42d3a878a
barinderbanwait <b.s.banwait86@gmail.com>
2021-03-05 17:03:51 +0530

    status update

--- magma/utils.m
+++ magma/utils.m
@@ -19,3 +19,13 @@ if r[2] ge 1 then A:=A join {d}; end if;
 end for;
 return A;
 end function;
+
+// Code for 65
+E := EllipticCurve([1, 0, 0, -1, 0]);
+DescentInformation(QuadraticTwist(E,5));
+
+
+// Code for 125:
+R<x> := PolynomialRing(Rationals());
+C := HyperellipticCurve(R![0, 0, 1, 2, 2, 2], R![1, 1, 0, 1]);
+X,pi:= SimplifiedModel(C);
