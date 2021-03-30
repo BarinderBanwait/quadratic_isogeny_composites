c32bd8ab3cba87b0e9e0749bae1b630c59ee3ecb
barinderbanwait <b.s.banwait86@gmail.com>
2021-03-30 11:58:58 +0530

    Genus computation for quotient modular curves, and some WIP for quadratic point determination

--- magma/utils.m
+++ magma/utils.m
@@ -65,3 +65,100 @@ Evaluate(f,[1728,j1]);
 
 // Code for 91:
 R<x> := PolynomialRing(Rationals());
+C := HyperellipticCurve(x^6 + 2*x^5 - x^4 - 8*x^3 - x^2 + 2*x +1);  // X0(91)+
+J := Jacobian(C);
+
+load "ozmansiksek.m";
+
+ModCrvQuot:=function(N,wlist,remwlist)
+C:=CuspForms(N);
+ws:=[AtkinLehnerOperator(C,n) : n in wlist];
+remws:=[AtkinLehnerOperator(C,n) : n in remwlist];
+W:=AtkinLehnerOperator(C,N);
+NN:=&meet([Nullspace(Matrix(w-1)) : w in ws] cat [Nullspace(1-W^2)]);
+dim:=Dimension(NN);
+seqs:=[[Coordinates(NN,Basis(NN)[i]*Matrix(w)) : i in [1..dim]] : w in remws];
+BB:=[&+[(Integers()!(2*Eltseq(Basis(NN)[i])[j]))*C.j : j in [1..Dimension(C)]] : i in [1..dim]];
+prec:=500;
+L<q>:=LaurentSeriesRing(Rationals(),prec);
+R<[x]>:=PolynomialRing(Rationals(),dim);
+Bexp:=[L!qExpansion(BB[i],prec) : i in [1..dim]];
+eqns:=[R | ];
+	d:=1;
+	tf:=false;
+	while tf eq false do
+		d:=d+1;
+		mons:=MonomialsOfDegree(R,d);
+		monsq:=[Evaluate(mon,Bexp) : mon in mons];
+		V:=VectorSpace(Rationals(),#mons);
+		W:=VectorSpace(Rationals(),prec-10);
+		h:=hom<V->W | [W![Coefficient(monsq[i],j) : j in [1..(prec-10)]] : i in [1..#mons]]>;
+		K:=Kernel(h);
+		eqns:=eqns cat [ &+[Eltseq(V!k)[j]*mons[j] : j in [1..#mons] ] : k in Basis(K)  ];
+        I:=Radical(ideal<R | eqns>);
+		X:=Scheme(ProjectiveSpace(R),I);
+		if Dimension(X) eq 1 then
+			if IsSingular(X) eq false then
+				X:=Curve(ProjectiveSpace(R),eqns);
+				if Genus(X) eq dim then
+					tf:=true;
+				end if;
+			end if;
+		end if;
+	end while;
+	eqns:=GroebnerBasis(ideal<R | eqns>); // Simplifying the equations.
+	tf:=true;
+	repeat
+		t:=#eqns;
+		tf:=(eqns[t] in ideal<R | eqns[1..(t-1)]>);
+		if tf then
+			Exclude(~eqns,eqns[t]);
+		end if;
+	until tf eq false;
+	t:=0;
+	repeat
+		t:=t+1;
+		tf:=(eqns[t] in ideal<R | Exclude(eqns,eqns[t])>);
+		if tf then
+			Exclude(~eqns,eqns[t]);
+			t:=0;
+		end if;
+	until tf eq false and t eq #eqns;
+	X:=Curve(ProjectiveSpace(R),eqns); // Our model for X_0(N) discovered via the canonical embedding.
+	assert Genus(X) eq dim;
+	assert IsSingular(X) eq false;
+    indexGam:=N*&*[Rationals() | 1+1/p : p in PrimeDivisors(N)];
+	indexGam:=Integers()!indexGam; // Index of Gamma_0(N) in SL_2(Z)
+	for eqn in eqns do
+		eqnScaled:=LCM([Denominator(c) : c in Coefficients(eqn)])*eqn;
+		wt:=2*Degree(eqn); // Weight of eqn as a cuspform.
+		hecke:=Ceiling(indexGam*wt/12);  // Hecke=Sturm bound.
+										// See Stein's book, Thm 9.18.
+		Bexp1:=[qExpansion(BB[i],hecke+10) : i in [1..dim]]; // q-expansions
+                        // of basis for S
+                        // up to precision hecke+10.
+		assert Valuation(Evaluate(eqnScaled,Bexp1)) gt hecke+1;
+	end for; // We have now checked the correctness of the equations for X.
+seqlist:=[[&+[seq[i][j]*x[j] : j in [1..dim]] : i in [1..dim]] : seq in seqs];
+wmaplist:=[iso<X->X | seq,seq> : seq in seqlist];
+return X,wmaplist,seqs;
+end function;
+
+
+X,ws:=ModCrvQuot(91,[],[91]); //Just a few seconds.
+w:=ws[1];
+assert Genus(X) eq 7;
+
+//This function computes J_X(F_p).
+JacobianFp:=function(X)
+CC,phii,psii:=ClassGroup(X); //Algorithm of Hess
+Z:=FreeAbelianGroup(1);
+degr:=hom<CC->Z | [ Degree(phii(a))*Z.1 : a in OrderedGenerators(CC)]>;
+JFp:=Kernel(degr); // This is isomorphic to J_X(\F_p).
+return JFp,phii,psii;
+end function;
+
+cusps:=PointSearch(X,500);
+numcusps := #cusps;
+Dtors:=[Place(cusps[i])-Place(cusps[1]) : i in [2..4]];
+
--- utils.py
+++ utils.py
@@ -14,3 +14,82 @@ def nonsplit_cartan_genus(p):
     non-split Cartan modular curves"""
 
     return (1/24) * (p^2 - 10*p + 23 + 6 * kronecker_symbol(-1,p) + 4 * kronecker_symbol(-3,p))
+
+
+def is_atkin_lehner_divisor(d, N):
+
+    if d.divides(N):
+        if gcd(d, N/d) == 1:
+            return True
+    return False
+
+def c_i_at_2(i, Ndash, N):
+
+    if i == 1:
+        if Ndash%4 == 1:
+            if is_atkin_lehner_divisor(2, N):
+                return 1
+            elif 4.divides(N):
+                return 0
+        if Ndash%4 == 3:
+            if is_atkin_lehner_divisor(2,N):
+                return 2
+            elif 4.divides(N):
+                return 3 + kronecker_symbol(-Ndash,2)
+            elif 8.divides(N):
+                return 3 * (1 + kronecker_symbol(-Ndash,2))
+
+    if i == 2 and Ndash%4 == 3:
+        return 1 + kronecker_symbol(-Ndash,2)
+
+
+def c_i(i, p, Ndash, N):
+
+    if p == 2:
+        return c_i_at_2(i, Ndash, N)
+
+    if Ndash%4 == 3:
+        return 1 + kronecker_symbol(-Ndash, p)
+
+    else:
+        return 1 + kronecker_symbol(-4*Ndash, p)
+
+def fixed_point_number(Ndash,N):
+
+    assert is_atkin_lehner_divisor(Ndash,N), "Ndash does not give an AL involution"
+
+    N_over_Ndash = ZZ(N/Ndash)
+
+    base_contribution = prod([c_i(1, p, Ndash, N) for p in N_over_Ndash.prime_divisors()]) * (-4*Ndash).class_number()
+
+    if Ndash == 2:
+        other_contribution = prod([1 + kronecker_symbol(-4,p) for p in ZZ(N/2).prime_divisors()])
+    elif Ndash == 3:
+        other_contribution = prod([1 + kronecker_symbol(-3,p) for p in ZZ(N/3).prime_divisors()])
+    elif Ndash == 4:
+        all_divisors_N_over_4 = (ZZ(N/4)).divisors()
+        all_AL_divisors = [D for D in all_divisors_N_over_4 if is_atkin_lehner_divisor(D,(ZZ(N/4)))]
+        pre_prime_power_divisor_data = [D.is_prime_power(get_data=True) for D in all_AL_divisors]
+        prime_power_divisor_data = [(p,nu) for p,nu in pre_prime_power_divisor_data if nu != 0]
+        other_contribution = prod([p**(nu // 2) + p**((nu-1) // 2) for p,nu in prime_power_divisor_data])
+    elif Ndash%4 == 3:
+        other_contribution = prod([c_i(2, p, Ndash, N) for p in N_over_Ndash.prime_divisors()]) * (-Ndash).class_number()
+    else:
+        other_contribution = 0
+
+    return base_contribution + other_contribution
+
+def genus_of_quotient(N, Ndash):
+
+    return (1/4) * (2 * Gamma0(N).genus() + 2 - fixed_point_number(Ndash, N))
+
+
+# Some testing of the function, the following examples taken from Galbraith's thesis
+
+assert genus_of_quotient(91,91) == 2
+assert genus_of_quotient(46,2) == 3
+assert genus_of_quotient(55, 5) == 3
+assert genus_of_quotient(56, 2) == 3
+assert genus_of_quotient(84,84) == 4
+assert genus_of_quotient(92,23) == 1
+assert genus_of_quotient(99,99) == 3
