3ce33c55e4d9ad1824a042aa15ab4a0d259e074a
barinderbanwait <b.s.banwait86@gmail.com>
2022-07-10 14:29:59 +0200

    failed dict v0.1

--- sage_code/isogeny_graphs.py
+++ sage_code/isogeny_graphs.py
@@ -64,8 +64,11 @@ def isogeny_class_via_sage(j, K, d):
         try:
             logger.info("OK so now trying the gp thing")
             L,M = isogeny_class_via_gp(j, K, d)
+            logger.info("yay that worked!! :)")
             return L, M
         except TimeoutError:
+            signal.signal(signal.SIGALRM, handler)
+            signal.alarm(ISOGENY_CLASS_TIMEOUT_S)
             # now we really can't do anything more
             logger.info("oh dear oh dear oh dear")
             return None, None
